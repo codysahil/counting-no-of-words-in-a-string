1ddc15a082c612f54ca07f527ffd94853bdd184b
Sahil Raj <98708331+codysahil@users.noreply.github.com>
2022-11-23 22:30:45 +0530

    Update ConsoleApplication53.cpp

--- ConsoleApplication53/ConsoleApplication53.cpp
+++ ConsoleApplication53/ConsoleApplication53.cpp
@@ -1,20 +1,30 @@
-// ConsoleApplication53.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#include<iostream>
+using namespace std;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+void cnt(char arr[]) {
+	int vcount = 0;
+	for (int i = 0; arr[i] != '\0'; i++) {
+		if (arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u' || arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U') {
+			vcount++;
+		}
+	}
+	cout << vcount<<endl;
+}
+void cntwords(char arr[]) {
+	int word = 0;
+	for (int i = 0; arr[i] != '\0'; i++) {
+		if (arr[i] == ' ' && arr[i] != '\0') {
+			word++;
+		}
+	}
+	cout << word+1;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+	char arr[] = "welcOmE Home bittu have a nice day";
+	cnt(arr);
+	cntwords(arr);
+
+	return 0;
+}
